93a9e279dc6856b1db7cacdc04441094a825517b
Darigaf <mateusbernardo@protonmail.com>
2022-10-06 17:34:17 -0300

    added camera functionality

--- python_gui/display.py
+++ python_gui/display.py
@@ -1,5 +1,9 @@
+#from tkinter import Tk, Label, Pack
 from tkinter import *
 from tkinter import ttk
+from PIL import ImageTk, Image
+#import PIL
+import cv2
 
 #creates the screen and puts the text in the title
 root = Tk()
@@ -8,25 +12,39 @@ root.geometry("1280x720")
 
 #creates the main frame, that will contain all other aplications widgets, and set it's size to 1280x720
 mainframe = ttk.Frame(root, padding="3 3 12 12")
-mainframe.pack(fill="both")
+mainframe.pack()
 #places the mainframe on the screen grid
 #mainframe.grid(column=0, row=0)
 #mainframe.columnconfigure(0, weight=10)
 #mainframe.rowconfigure(0, weight=10)
 
+camera_drone = Label(mainframe)
+video = cv2.VideoCapture(0)
+video_size = (1280, 720)
+def show_video():
+    camera = cv2.cvtColor(video.read()[1],cv2.COLOR_BGR2RGB)
+    camera = cv2.resize(camera, video_size)
+    imagem = Image.fromarray(camera)
+    imgtk = ImageTk.PhotoImage(image = imagem)
+    camera_drone.imgtk = imgtk
+    camera_drone.configure(image=imgtk)
+    camera_drone.after(20, show_video)
+
+
 #creates the camera frame, the plan is to display the live tello camera on it
 #camera = ttk.Frame(mainframe, width=1280, height=720)
 
+camera_drone.pack()
 controler = ttk.Frame(mainframe, height=72)
 controler_wasd = ttk.Frame(controler)
 
 #controler.grid(column=0, row=1, sticky=(S))
-controler.pack(side="bottom", fill=X)
+controler.pack(side=BOTTOM)
 #controler.rowconfigure(0, weight=5)
 #controler.columnconfigure(0, weight=5)
 
 #controler_wasd.grid(column=0, row=1, sticky=(S, W))
-controler_wasd.pack(side="left")
+controler_wasd.pack(side=LEFT)
 #controler_wasd.rowconfigure(0)
 #controler_wasd.columnconfigure(0)
 
@@ -36,10 +54,10 @@ controler_a = ttk.Button(controler_wasd, text='A')
 controler_s = ttk.Button(controler_wasd, text='S')
 controler_d = ttk.Button(controler_wasd, text='D')
 
-controler_w.pack(anchor="n")
-controler_a.pack(anchor="sw")
-controler_s.pack(anchor="s")
-controler_d.pack(anchor="se")
+controler_w.pack()
+controler_a.pack()
+controler_s.pack()
+controler_d.pack()
 
 
 #places the wasd buttons on their default layout
@@ -50,4 +68,5 @@ controler_d.pack(anchor="se")
 
 #controler_up_down = ttk.Frame(controler)
 #controler_land_takeoff = ttk.Frame(controler)
+show_video()
 root.mainloop()
